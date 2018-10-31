e33554cf1e8448a5d9a1092bdedb48153731666d
Jose de Assis <jassis@cgctec.com.br>
2018-10-31 19:37:41 -0300

    Adicionados Bootstrap, Material Designer, jQuery, Font Awesome, jQuery-ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare var $: any;
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'barcelona';
+export class AppComponent implements OnInit {
+  title = 'Barcelona';
+
+  ngOnInit() {
+    $(function () {
+      $('#sortable').sortable().disableSelection();
+      $('#btn1').click(function() {
+        $('#hide').toggle('hide');
+      });
+    });
+  }
 }
