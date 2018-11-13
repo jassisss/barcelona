44b3fb4ce0b9754d78a077539f7279657a87077a
Jose de Assis <jassis@cgctec.com.br>
2018-11-13 12:34:34 -0200

    Ajuste na cor do Logo e inclusão do botão para scroll up

--- src/app/site/components/main/main.component.ts
+++ src/app/site/components/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let $ : any;
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -10,6 +12,25 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // BACK TO TOP BUTTON ///////////////////////////////////////////////////////////////////////////////////////////////////////
+    $(document).ready(function(){
+      $(window).on('scroll', function(){
+        if ( $(this).scrollTop() > 300 ) {
+          $('#back_top').fadeIn("slow");
+        }
+        else {
+          $('#back_top').fadeOut("slow");
+        }
+      });
+
+      $('#back_top').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate( {scrollTop : 0}, 800 );
+        $('#back_top').fadeOut("slow").stop();
+      });
+
+    });
+
   }
 
 }
